90808e4b14f61456da15b9598f45ec04dfb13d2b
unknown <chipsfu@gmail.com>
2019-07-25 22:11:52 +0700

    ok

---
+++ main.js
@@ -0,0 +1,50 @@
+
+function openStream() {
+    const config = { audio: false, video: true };
+    return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag, stream) {
+    const video = document.getElementById(idVideoTag);
+    video.srcObject = stream;
+    video.play();
+}
+
+//   openStream()
+//   .then(stream => playStream('localStream', stream));
+ const peer = new Peer({ 
+    key: 'peerjs', 
+    host: 'peerserverstream.herokuapp.com', 
+    secure: true, 
+    port: 443, 
+    config: customConfig 
+});
+
+peer.on('open', id => {
+    $('#my-peer').append(id);
+    // $('#btnSignUp').click(() => {
+    //     const username = $('#txtUsername').val();
+    //     socket.emit('NGUOI_DUNG_DANG_KY', { ten: username, peerId: id });
+    // });
+});
+
+//Caller
+$('#btnCall').click(() => {
+    const id = $('#remoteId').val();
+    openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
+
+//Callee
+peer.on('call', call => {
+    openStream()
+    .then(stream => {
+        call.answer(stream);
+        playStream('localStream', stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
